a663cc55270a760e11fd488b4ab78d4ffa2591ef
Justyna Pruś <65462428+JustynaPrus@users.noreply.github.com>
2021-11-01 16:48:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form");
+const nameInput = document.getElementById("nameInput");
+const emailInput = document.getElementById("emailInput");
+const checkbox = document.getElementById("checkbox");
+const popup = document.getElementById("popup");
+const mainSide = document.getElementById("main_side");
+const yourName = document.getElementById("yourName");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let nameInputValue = nameInput.value;
+  let emailInputValue = emailInput.value;
+
+  if (emailInputValue == "" || nameInputValue == "") {
+    alert("Halo! Email i imię nie mogą byc puste!");
+  } else if (checkbox.checked == false) {
+    alert("Uwaga! Warunki muszą być zaakceptowane!");
+  } else if (emailInputValue != "" && nameInputValue != "") {
+  }
+
+  mainSide.classList.toggle("inactive");
+  popup.classList.toggle("inactive");
+  yourName.innerHTML = `${nameInputValue}`;
+});
